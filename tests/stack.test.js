98837b05bc21eeeca9ddbdd71f56dc293c2f98b3
ShahoBradostin <Shahobradostin@gmail.com>
2025-05-09 19:20:42 +0200

    failed tests

---
+++ tests/stack.test.js
@@ -0,0 +1,31 @@
+const stack = require('../src/stack');
+
+test('peek on empty stack returns undefined', () => {
+    expect(stack.peek()).toBeUndefined();
+});
+
+test('peek on stack with one element returns that element', () => {
+    stack.push(1);
+    expect(stack.peek()).toBeDefined();
+    expect(stack.peek()).toBe(1);
+});
+
+test('peek on stack with two or more elements returns the top element', () => {
+    stack.push(1);
+    stack.push("wow");
+    stack.push(42);
+    expect(stack.peek()).toBeDefined();
+    expect(stack.peek()).toBe(42);
+});
+
+// Add this to stack.test.js
+test('pop on stack with elements removes and returns the top element', () => {
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+    expect(stack.pop()).toBe(3);
+    expect(stack.peek()).toBe(2);
+    expect(stack.pop()).toBe(2);
+    expect(stack.pop()).toBe(1);
+    expect(stack.pop()).toBeUndefined(); // Empty stack now
+});
